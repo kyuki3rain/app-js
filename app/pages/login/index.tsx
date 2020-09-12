a00417c7e6ca1ad4b13f05ac43eb6c68c29f9825
yuki koro <kyuki3rain@gmail.com>
2020-09-12 22:33:25 +0900

    first-next-app

---
+++ app/pages/login/index.tsx
@@ -0,0 +1,35 @@
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+
+const Login = () => {
+  return (
+    <Container className="login">
+      <Row>
+        <Col className="text-center">
+          <h2>ログイン</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form className="col-6 offset-3">
+            <Form.Group>
+              <Form.Control type="text" name="id" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="password" name="password" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control
+                type="submit"
+                name="submit"
+                value="ログイン"
+                className="btn btn-primary"
+              />
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Login
---
+++ doc.md
@@ -0,0 +1,35 @@
+# markdown を書きたい！
+
+今まで一度も書いたことがなかった、あの憧れの markdown を、ついに書くことになりました。
+
+まずは環境構築。
+
+vscode で書きたいのは決まっていたので調べてみると、なんと標準機能でプレビューができるらしい。
+
+[任意のファイル名].md という名前のファイルを生成したら、それを開いて Ctrl + Shift + v でプレビューが見れた。
+
+# Next.js + TypeScript + Express + Jest
+
+https://dev.classmethod.jp/articles/graphql-tutorial-nodejsexpress/
+
+https://serip39.hatenablog.com/entry/2020/06/20/234000
+
+```
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+```
+
+https://qiita.com/atlansien/items/c587a0bf2f7f9022107c
+https://qiita.com/anoonoll/items/050ebb62143ada022a32
+
+docker-compose up --build
+
+nodemon
+https://qiita.com/twipg/items/cb969b335d66c4aee690
+https://qiita.com/ckoshien/items/1a8b15fe5cc3bfc15199
+
+https://genzouw.com/entry/2020/01/28/120014/1910/#toc_id_4
+
+# nextjs
+
+https://page.ak2ie.net/2019/06/17/nextjs-bootstrap/
